d75d8b895529c328220a51e4b3bd3dfe979cd5e6
Miguel <protoblae@gmail.com>
2022-01-14 00:04:20 +0900

    challenge 4 complete

--- 7-Hangman.py
+++ 7-Hangman.py
@@ -6,6 +6,65 @@ word_as_list = list(chosen_word)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
+
+lives = 6
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 display = []
 
 for i in chosen_word:
@@ -14,16 +73,29 @@ for i in chosen_word:
 print(display)
 #print(len(chosen_word))
 
+end_game = False
 
-
-while display != word_as_list:
+while not end_game:
+    print(f"Lives: {lives}")
+    print(stages[lives])
+    print(display)
+    
     guess = input("Guess a letter: ").lower()
 
     for dis_num in range(len(chosen_word)): #loops the number of times as there are letters in the chosen word from the list
         letter = chosen_word[dis_num] #starting at the first letter of the chosen word, will check the index (chosen_word[the current letter of that word])
         if letter == guess: # if the letter and guess match
             display[dis_num] = guess #then the display[] index will change to match the letter at that position
+    
+    if guess not in display:
+        lives -= 1
+    
+    
 
-    print(display)
-
-print("Game Over")
+    if display == word_as_list:
+        end_game = True
+        print("You win!")
+    elif lives == 0:
+        end_game = True
+        print("You lose!")
+    
