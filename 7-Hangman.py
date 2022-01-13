94fd53357adca4b80e5c09699bd7f997f725bccb
Miguel <protoblae@gmail.com>
2022-01-13 14:26:04 +0900

    Challenge one tasks done

--- 7-Hangman.py
+++ 7-Hangman.py
@@ -1,18 +1,21 @@
 import random
-#Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 chosen_word = random.choice(word_list)
 
+word = ""
+
+print(chosen_word)
+
 guess = input("Choose a letter: ").lower()
 
-chosen_list = list(chosen_word)
+num = 0
 
-print(chosen_list)
+for letter in chosen_word:
+    if letter == guess:
+        print("yes")
+    else:
+        print("no")
+    num += 1
