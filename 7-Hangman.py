62ab3cb06b1ec23d71e33ebbe71da5e9ef52ef74
Miguel <protoblae@gmail.com>
2022-01-13 23:18:57 +0900

    challenge 3 finished

--- 7-Hangman.py
+++ 7-Hangman.py
@@ -2,7 +2,7 @@ from dis import dis
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+word_as_list = list(chosen_word)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -14,14 +14,16 @@ for i in chosen_word:
 print(display)
 #print(len(chosen_word))
 
-guess = input("Guess a letter: ").lower()
 
-for dis_num in range(len(chosen_word)): #loops the number of times as there are letters in the chosen word from the list
-    letter = chosen_word[dis_num] #starting at the first letter of the chosen word, will check the index
-    if letter == guess: # if the letter and guess match
-        print("Right")
-        display[dis_num] = guess #then the display[] index will change to match the letter at that position
-    else:
-        print("Wrong")
 
-print(display)
+while display != word_as_list:
+    guess = input("Guess a letter: ").lower()
+
+    for dis_num in range(len(chosen_word)): #loops the number of times as there are letters in the chosen word from the list
+        letter = chosen_word[dis_num] #starting at the first letter of the chosen word, will check the index (chosen_word[the current letter of that word])
+        if letter == guess: # if the letter and guess match
+            display[dis_num] = guess #then the display[] index will change to match the letter at that position
+
+    print(display)
+
+print("Game Over")
